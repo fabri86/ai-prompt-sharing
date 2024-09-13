edb4a6e9226ddf79acd2027ae192037e8c21b33a
fabri86 <ing.fabrizioiacovone@gmail.com>
2024-09-13 18:08:43 +0200

    Fetch prompts and copy prompt to clipboard

--- components/feed.tsx
+++ components/feed.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+'use client'
+
+import { useState, useEffect } from 'react'
+import PromptCard from '@components/prompt-card'
+
+type PromptCardListProps = {
+  data: Post[]
+  handleTagClick: () => void
+}
+
+const PromptCardList = ({ data, handleTagClick }: PromptCardListProps) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post: Post, index: number) => (
+        <PromptCard key={post.id || `post-${index}`} post={post} handleTagClick={handleTagClick} />
+      ))}
+    </div>
+  )
+}
 
 const Feed = () => {
-  return <div>A feed</div>
+  const [searchText, setSearchText] = useState<string>('')
+  const [posts, setPosts] = useState<Post[]>([])
+
+  const handleSearchChange = (value: string) => {}
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt')
+      const data: Post[] = await response.json()
+
+      setPosts(data)
+    }
+
+    fetchPosts()
+  }, [])
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          onChange={(e) => handleSearchChange(e.target.value)}
+          required
+          value={searchText}
+          className="search_input peer"
+        ></input>
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  )
 }
 
 export default Feed
